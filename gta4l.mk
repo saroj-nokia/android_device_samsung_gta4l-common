66c11ae7ecd8a498ebb27c546155470e0d298e35
roynatech2544 <whiteshell2544@naver.com>
2023-12-10 14:53:37 +0900

    gta4l-common: Drop qcom perf stack
    - It wasn't even operative in the first place due to Samsung disabling its functionailty practically
    
    Signed-off-by: CuriousNom <aryanbiswas7585@gmail.com>

--- gta4l.mk
+++ gta4l.mk
@@ -283,10 +283,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power@1.2.vendor
 
 # Protobuf
 PRODUCT_PACKAGES += \
