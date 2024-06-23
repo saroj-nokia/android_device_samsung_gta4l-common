bca344d16bc7e5dcc4dc58d54d500aa903d4a216
Bruno Martins <bgcngm@gmail.com>
2024-06-23 12:29:15 +0100

    gta4l-common: Move to new RFS install_symlink targets
    Change-Id: I9560fd0da00958d9349a0881b5a5f64a2c02e495

--- gta4l.mk
+++ gta4l.mk
@@ -23,6 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Add common definitions for Qualcomm
+$(call inherit-product, hardware/qcom-caf/common/common.mk)
+
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/gta4l-common/gta4l-common-vendor.mk)
 
