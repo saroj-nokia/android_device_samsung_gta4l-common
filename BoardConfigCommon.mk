bfac6f2b5d051f09be7567b4627e6980da8b9099
Aryan Biswas <152459091+CuriousNom@users.noreply.github.com>
2024-07-21 14:21:24 +0530

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,6 +88,7 @@ TARGET_USES_ION := true
 USE_OPENGL_RENDERER := true
 
 # HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
